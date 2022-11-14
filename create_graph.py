8c1a641550e5a66cdcd3501431c56c556c864dad
Jason Conley <70329226+jasonbconley@users.noreply.github.com>
2022-11-14 16:59:39 -0500

    Create graph script with node cleaning

--- create_graph.py
+++ create_graph.py
@@ -22,6 +22,12 @@ def read_node_file():
     return nodes
 
 
+def get_node_list():
+    nodes = pd.read_csv('wiki_data/articles.tsv')
+    nodes["Article_Name"] = nodes["Article_Name"].apply(format_string)
+    return nodes["Article_Name"]
+
+
 def create_graph():
     nodes = read_node_file()
     graph = get_edge_file()
@@ -29,10 +35,20 @@ def create_graph():
     return graph
 
 
+# Interesting problem, need to find a community of nodes that are isolated in a group of 3
 def main():
     wiki_network = create_graph()
 
-    print(wiki_network.number_of_edges())
+    col_names = ['src', 'dest']
+    links = pd.read_csv("wiki_data/links.tsv", delimiter='\t', names=col_names)
+    links["src"] = links["src"].apply(format_string)
+    links["dest"] = links["dest"].apply(format_string)
+
+    # Found these through a long an arduous programming process that resulted in me just staring at the dataset
+    # Here are the three nodes that formed the disconnected community
+    wiki_network.remove_node("Friend_Directdebit")
+    wiki_network.remove_node("Directdebit")
+    wiki_network.remove_node("Sponsorship_Directdebit")
 
     nx.write_gml(wiki_network, "./wiki_data/wiki_network.gml")
 
